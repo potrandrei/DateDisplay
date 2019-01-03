3ef356e6ef248fe8043c89ba7e6f685f652a93b7
potrandrei <43883781+potrandrei@users.noreply.github.com>
2019-01-03 15:01:50 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,14 @@
+package DisplayDate;
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ){
+
+Date date = new Date();
+        System.out.println(date.toString());
+    }
+}
